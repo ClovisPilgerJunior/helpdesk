8aab6343e3b38e84c870b149e5d40a298689c029
Clovis Pilger Junior <clovispilgerjunior@hotmail.com>
2023-07-08 18:02:59 -0300

    Inicalizate projetct

---
+++ src/test/java/com/app/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
