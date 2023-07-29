4e241afe6debcf40ee500239b8314a216e1436ff
Clovis Pilger Junior <clovispilgerjunior@hotmail.com>
2023-07-29 19:34:13 -0300

    Changed minor configuration in securityFilterChain

--- src/main/java/com/app/helpdesk/config/SecurityConfig.java
+++ src/main/java/com/app/helpdesk/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(authorize -> authorize
             .requestMatchers(HttpMethod.POST,"/auth/login").permitAll()
-                .requestMatchers(HttpMethod.GET, "/").permitAll()
+//                .requestMatchers(HttpMethod.GET, "/").permitAll()
 //            .requestMatchers(HttpMethod.POST,"/auth/register").permitAll()
 //            .requestMatchers(HttpMethod.POST,"/ticket").hasRole("ADMIN")
                 .requestMatchers(toH2Console()).permitAll()
